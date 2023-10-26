95bfa04e2909cf2890b7033b0886efe6d188a537
Gorka Sanz <gorka.sanz@salesianos.edu>
2023-10-26 12:39:26 +0200

    added an api  constraint for edad field

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from odoo import api, models, fields, api
+from odoo import api, models, fields
 
 class Cliente(models.Model):
     _name = 'salesianos.cliente'
@@ -6,6 +6,18 @@ class Cliente(models.Model):
     email = fields.Char()
     phone = fields.Char()
     vendedor = fields.Many2one(comodel_name='salesianos.vendedor', string='vendedor')
+    edad = fields.Integer()
+
+    @api.constrains('edad', 'name')
+    def _check_edad(self):
+        # solo se ejecuta una vez
+        for record in self:
+            if record.edad < 18:
+                raise models.ValidationError("El cliente "+record.name+" debe ser mayor de edad")
+            elif record.edad > 100:
+                raise models.ValidationError("El cliente "+record.name+"  no puede ser tan viejo")
+            else:
+                pass
 
 class Vendedor(models.Model):
     _name = 'salesianos.vendedor'
